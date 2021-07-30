068120e7b2885e9a6184ab1ed0ac92cd4a9f5f01
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-07-30 13:46:51 -0300

    configuração do Form

--- email.php
+++ email.php
@@ -1,16 +1,16 @@
 <?php
 
-$nome = addcslashes($_POST(['name']))
-$cpf = addcslashes($_POST(['cpf']))
-$$celular = addcslashes($_POST(['celular']))
-$concorda = addcslashes($_POST(['concorda']))
+$nome = addcslashes($_POST['name']);
+$cpf = addcslashes($_POST['cpf']);
+$$celular = addcslashes($_POST['celular']);
+$concorda = addcslashes($_POST['concorda']);
 
 $sto = "vandessa@vemconsig.com.br";
 $subject = "Cliente - Site VEMCONSIG";
-$body = "Nome: ".$nome. "\n"
-        "CPF: ".$cpf. "\n"
-        "Celular: ".$celular. "\n"
-        "Concorda: ".$concorda. "\n"
+$body = "Nome: ".$nome. "\n".
+        "CPF: ".$cpf. "\n".
+        "Celular: ".$celular. "\n".
+        "Concorda: ".$concorda. "\n";
 $header = "From: atendimento@vemconsig.com"
             ."Replay-To:".$email."\r\n"
             ."X=Mailer:PHP/".phpversion();
