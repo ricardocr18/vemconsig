7e77b87dacd2d660cefd53eeabfceda75177e2b3
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-07-30 15:28:30 -0300

    configuração do Form

--- email.php
+++ email.php
@@ -2,20 +2,20 @@
 
 $nome = addcslashes($_POST['name']);
 $cpf = addcslashes($_POST['cpf']);
-$$celular = addcslashes($_POST['celular']);
+$celular = addcslashes($_POST['celular']);
 $concorda = addcslashes($_POST['concorda']);
 
-$sto = "vandessa@vemconsig.com.br";
+$to = "vandessa@vemconsig.com.br";
 $subject = "Cliente - Site VEMCONSIG";
 $body = "Nome: ".$nome. "\n".
         "CPF: ".$cpf. "\n".
         "Celular: ".$celular. "\n".
         "Concorda: ".$concorda. "\n";
-$header = "From: atendimento@vemconsig.com"
-            ."Replay-To:".$email."\r\n"
+$header = "From: atendimento@vemconsig.com"."\n"
+            ."Reply-To:".$email."\r\n"
             ."X=Mailer:PHP/".phpversion();
 
-if (mail($sto,$subject,$body,$header)){
+if (mail($to,$subject,$body,$header)){
     echo("Email enviado com sucesso");
 }else{
     echo("O Email não pode ser enviado");
